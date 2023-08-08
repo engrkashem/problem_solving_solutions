f0ee8f710c2d33e98b9a31375852b1f408bc8075
engrkashem <kashemaust@gmail.com>
2023-08-08 15:48:02 +0600

    post or reverse polished notation leetcode 150 added

--- Leetcode/postfix_reverse_polished_notation_155.py
+++ Leetcode/postfix_reverse_polished_notation_155.py
@@ -1,38 +1,37 @@
-# import math
-
 def evalRPN(tokens):
-    stack=[]
-    
-    for i in tokens:
-        
-        if i=='+':
-            top=int(stack.pop(-1))
-            secondTop=int(stack.pop(-1))
-            stack.append(top+secondTop)
-        elif i=='-':
-            top=int(stack.pop(-1))
-            secondTop=int(stack.pop(-1))
-            stack.append(secondTop-top)
-        elif i=='*':
-            top=stack.pop(-1)
-            secondTop=stack.pop(-1)
-            stack.append(int(top)*int(secondTop))
-        elif i=='/':
-            top=stack.pop(-1)
-            secondTop=stack.pop(-1)
-            stack.append(int(secondTop)/int(top))
-        else:
-            stack.append(i)
-        # print(stack)
-    
-    return stack[0]
-
+	stack=[]
+	
+	for i in tokens:
+		if i=='*':
+			stack.append(stack.pop()*stack.pop())
+		elif i=='/':
+			top=stack.pop()
+			second=stack.pop()
+			tf=1
+			sf=1
+			if top<0:
+				tf=-1
+			if second<0:
+				sf=-1
+			divRes=abs(second)//abs(top)
+			stack.append(divRes*tf*sf)
+		elif i=='+':
+			stack.append(stack.pop()+stack.pop())
+		elif i=='-':
+			top=stack.pop()
+			stack.append(stack.pop()-top)
+		else:
+			stack.append(int(i))
+	
+	return stack[-1]
+			
 
 # tokens = ["4","13","5","/","+"]
 # tokens = ["2","1","+","3","*"]
 tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
 res=evalRPN(tokens)
 print(res)
+
 # print((6/(-132)))
 # print((6/(132)))
 # print(math.ceil((6/(-132))))
