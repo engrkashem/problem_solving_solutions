6e8b135bd16055bb63acfd882cf5a24be0890b76
engrkashem <kashemaust@gmail.com>
2023-08-08 16:37:18 +0600

    daily temperature 739 added

--- Leetcode/daily_temperature_739.py
+++ Leetcode/daily_temperature_739.py
@@ -1,20 +1,28 @@
 def dailyTemperatures(temperatures):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
-        # n=len(temperatures)
-        # ans=[0]*n
-        
-        # for i in range(n-1):
-        #     diff=temperatures[i+1]-temperatures[i]
-        #     if diff>0:
-        #         ans[i]=diff
-        
-        # print(ans)
-            
-        
-        
-temperatures = [73,74,75,71,69,72,76,73]
+        st=[]
+        op=[0]*len(temperatures)
+        for i,t in enumerate(temperatures):
+                if len(st)==0:
+                        st.append((t,i))
+                        continue
+                topTem=st[-1][0]
+                # topDay=st[-1][1]
+                while t>topTem and len(st)>0:
+                        if t<=st[-1][0]:
+                                break
+                        temday=st.pop()
+                        topTem=temday[0]
+                        day=temday[1]
+                        op[day]=i-day
+                        # print(i,day, topTem, t)
+                        
+                st.append((t,i))
+                # print(st)
+        return op
+
+     
+# temperatures = [73,74,75,71,69,72,76,73]
+# temperatures = [30,40,50,60]
+temperatures = [30,60,90]
 res=dailyTemperatures(temperatures)
 print(res)
