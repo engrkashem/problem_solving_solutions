55733ca04122f17f407e60b04c90182723afcfa0
engrkashem <kashemaust@gmail.com>
2023-08-10 14:41:39 +0600

    car fleet added

--- Leetcode/car_fleet853.py
+++ Leetcode/car_fleet853.py
@@ -1,22 +1,37 @@
 
 def carFleet( target, position, speed):
-    time={}
-    for i in range(len(position)):
-        pass
+    # x=sorted(zip(position, speed))
+    x=zip(position, speed)
+    y=list(x)
+    y.sort()
+    
+    # time = [float(target - p) / s for p, s in sorted(zip(position, speed))]
+    time=[]
+    for p,s in y:
+        time.append((float(target-p)/s))
+    
+    ans=0
+    curTime=0
+    for t in time[::-1]:
+        if t>curTime:
+            ans+=1
+            curTime=t
+    
+    return ans
 
 
 
-target = 12
-position = [10,8,0,5,3]
-speed = [2,4,1,1,3]
+# target = 12
+# position = [10,8,0,5,3]
+# speed = [2,4,1,1,3]
 
 # target = 10
 # position = [3]
 # speed = [3]
 
-# target = 100
-# position = [0,2,4]
-# speed = [4,2,1]
+target = 100
+position = [0,2,4]
+speed = [4,2,1]
 
 res=carFleet(target=target, speed=speed, position=position)
 print(res)
