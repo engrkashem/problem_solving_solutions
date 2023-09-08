8eedc1e87756f7bfab5e72b4b55a32b6403a4f78
engrkashem <kashemaust@gmail.com>
2023-09-08 21:10:19 +0600

    index of a sub string of first occurance

---
+++ Leetcode/index_first)coccurance28.py
@@ -0,0 +1,13 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        if needle in haystack:
+            ans=haystack.find(needle)
+            return ans
+        else:
+            return -1
+    
+obj=Solution()
+# haystack = "sadbutsad"; needle = "sad"
+# haystack = "leetcode"; needle = "leeto"
+haystack = "leetcode"; needle = "code"
+print(obj.strStr(haystack, needle))
