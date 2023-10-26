c851043d776b8a5837a91af037c2c44a17d6c2ac
engrkashem <kashemaust@gmail.com>
2023-10-26 08:57:55 +0600

    pacific atlan water flow optimised by 500+ms and 1md

--- Leetcode/pacific_atlantic_water_flow_417_optimised.py
+++ Leetcode/pacific_atlantic_water_flow_417_optimised.py
@@ -6,6 +6,7 @@ class Solution(object):
         output = []
         visited = set()
         direction = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        memory = set()
 
         def isInside(node):
             if node[0] >= 0 and node[0] < n and node[1] >= 0 and node[1] < m:
@@ -23,8 +24,12 @@ class Solution(object):
             if x == n-1 or y == m-1:
                 atlanticTouched[0] = True
 
+            if node in memory:
+                pacificTouched[0] = True
+                atlanticTouched[0] = True
+
             if atlanticTouched[0] and pacificTouched[0]:
-                return
+                return True
 
             for x1, y1 in direction:
                 newX = x+x1
@@ -34,11 +39,18 @@ class Solution(object):
                 if isInside(newNode) and newNode not in visited and heights[x][y] >= heights[newX][newY]:
                     dfs(newNode)
 
+            return False
+
         for i in range(n):
             for j in range(m):
+                if (i == 0 or j == 0) and (i == n-1 or j == m-1):
+                    output.append([i, j])
+                    continue
+
                 dfs((i, j))
                 if atlanticTouched[0] and pacificTouched[0]:
                     output.append([i, j])
+                    memory.add((i, j))
                 atlanticTouched[0] = False
                 pacificTouched[0] = False
                 visited.clear()
@@ -48,12 +60,12 @@ class Solution(object):
 
 obj = Solution()
 
-# heights = [[1, 2, 2, 3, 5],
-#            [3, 2, 3, 4, 4],
-#            [2, 4, 5, 3, 1],
-#            [6, 7, 1, 4, 5],
-#            [5, 1, 1, 2, 4]]
+heights = [[1, 2, 2, 3, 5],
+           [3, 2, 3, 4, 4],
+           [2, 4, 5, 3, 1],
+           [6, 7, 1, 4, 5],
+           [5, 1, 1, 2, 4]]
 
-heights = [[1]]
+# heights = [[1]]
 
 print(obj.pacificAtlantic(heights))
