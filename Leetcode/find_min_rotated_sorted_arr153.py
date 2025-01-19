c5aa0a217d8733837eb6cffaf946c4b8376d79e5
engrkashem <kashemaust@gmail.com>
2025-01-19 22:19:42 +0600

    perf:min in rotated sorted array 153 solved and improved to 0ms run time

--- Leetcode/find_min_rotated_sorted_arr153.py
+++ Leetcode/find_min_rotated_sorted_arr153.py
@@ -1,5 +1,46 @@
 class Solution:
     def findMin(self, nums):
+        result=10**4
+        left,right=0,len(nums)-1
+
+        while(left<=right):
+           
+            if nums[left]<=nums[right]:
+                result=min(result,nums[left])
+                break
+
+            mid=left+(right-left)//2
+
+            if nums[left]<nums[mid]:
+                result=min(result, nums[left])
+                left=mid+1
+                
+            elif nums[right]>nums[mid]:
+                result=min(result, nums[mid])
+                right=mid-1
+            else:
+                result=min(result,nums[right])
+                left=mid+1
+
+        return result
+
+
+                
+    
+obj=Solution()
+nums = [3,4,5,1,2]
+# nums = [4,5,6,7,0,1,2]
+# nums = [11,13,15,17]
+# nums=[2,3,4,5,1]
+# nums=[5,4,3,2,1]
+# nums=[2,3,1]
+# nums=[1]
+print(obj.findMin(nums))
+
+
+"""
+class Solution:
+    def findMin(self, nums):
         ans=10**5
         low,high=0,len(nums)-1
         
@@ -21,13 +62,6 @@ class Solution:
             
                 
         return ans
-                
-    
-obj=Solution()
-# nums = [3,4,5,1,2]
-# nums = [4,5,6,7,0,1,2]
-# nums = [11,13,15,17]
-# nums=[2,3,4,5,1]
-nums=[5,4,3,2,1]
-print(obj.findMin(nums))
+
+"""
         
