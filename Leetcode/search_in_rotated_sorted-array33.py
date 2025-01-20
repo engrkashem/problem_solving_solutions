330d99693386d6b4eb34370e3f077262eb249f69
engrkashem <kashemaust@gmail.com>
2025-01-20 15:12:20 +0600

    perf:aearch in a sorted rorated array 33 improved

--- Leetcode/search_in_rotated_sorted-array33.py
+++ Leetcode/search_in_rotated_sorted-array33.py
@@ -1,5 +1,36 @@
 class Solution:
     def search(self, nums, target):
+        left, right=0,len(nums)-1
+
+        while left<=right:
+            mid=left+(right-left)//2
+
+            if nums[mid]==target:
+                return mid
+            
+            if nums[left]<nums[mid]:
+                if nums[left]<=target and nums[mid]>target:
+                    right=mid-1
+                else:
+                    left = mid+1
+            else:
+                if nums[mid]<target and nums[right]>=target:
+                    left=mid+1
+                else:
+                    right=mid-1
+        return -1
+                
+    
+obj=Solution()
+nums = [4,5,6,7,0,1,2]; target = 0 #4
+# nums = [4,5,6,7,0,1,2]; target = 3 #-1
+# nums = [1]; target = 0 #-1
+# nums=[5,1,3]; target=3 #2
+print(obj.search(nums,target))
+
+"""
+class Solution:
+    def search(self, nums, target):
         low,high=0,len(nums)-1
         
         while low<=high:
@@ -19,11 +50,5 @@ class Solution:
                     high=mid-1
                 
         return -1
-                
-    
-obj=Solution()
-# nums = [4,5,6,7,0,1,2]; target = 0 #4
-# nums = [4,5,6,7,0,1,2]; target = 3 #-1
-# nums = [1]; target = 0 #-1
-nums=[5,1,3]; target=3 #2
-print(obj.search(nums,target))
+
+"""
