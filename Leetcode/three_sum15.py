939a13960e0eacec106d53a77f6b09fc5ad4aa7b
engrkashem <kashemaust@gmail.com>
2025-01-25 15:50:25 +0600

    perf:three sum solved again

--- Leetcode/three_sum15.py
+++ Leetcode/three_sum15.py
@@ -1,6 +1,47 @@
+import copy
 
 def threeSum( nums):
+    
     nums.sort()
+    result=[]
+
+    for i,n in enumerate(nums):
+        if n>0:
+            break
+
+        if i>0 and n== nums[i-1]:
+            continue
+
+        low, high=i+1, len(nums)-1
+        while low<high:
+            threeSum=nums[low]+n+nums[high]
+            if threeSum>0:
+                high -=1
+            elif threeSum<0:
+                low +=1
+            else:
+                result.append([nums[low],n,nums[high]])
+                low +=1
+                high -=1
+
+                while low<high and nums[low]==nums[low-1] :
+                    low +=1
+        
+    return result
+
+
+
+nums = [-1,0,1,2,-1,-4]
+# nums = [0,1,1]
+# nums = [0,0,0]
+
+
+res=threeSum(nums)
+print(res)
+
+
+"""
+nums.sort()
     ans=[]
     
     for i,n in enumerate(nums):
@@ -24,11 +65,4 @@ def threeSum( nums):
                     left+=1
     
     return ans
-
-
-# nums = [-1,0,1,2,-1,-4]
-# nums = [0,1,1]
-nums = [0,0,0]
-
-res=threeSum(nums)
-print(res)
+"""
