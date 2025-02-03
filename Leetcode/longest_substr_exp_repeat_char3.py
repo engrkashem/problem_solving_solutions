62d0c9f0b1aa753fda8b23f1f4e3b9bd35539950
engrkashem <kashemaust@gmail.com>
2025-02-03 20:04:05 +0600

    perflongest sub string 3 resolved

--- Leetcode/longest_substr_exp_repeat_char3.py
+++ Leetcode/longest_substr_exp_repeat_char3.py
@@ -1,5 +1,43 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
+        left=0
+        maxLength=0
+        strMap={}
+
+        for right in range(len(s)):
+            if s[right] in strMap:
+                left=max(left, strMap[s[right]] + 1)
+            
+            strMap[s[right]]=right
+            maxLength = max(maxLength, right-left+1)
+
+        return maxLength
+                    
+    
+# s = "abcabcbb"
+# s = "bbbbb"
+s = "pwwkew"
+obj=Solution()
+print(obj.lengthOfLongestSubstring(s))
+
+"""
+
+def lengthOfLongestSubstring(self, s):
+        left=0
+        maxLength=0
+        subStrSet=set()
+
+        for right in range(len(s)):
+            while s[right] in subStrSet:
+                subStrSet.remove(s[left])
+                left +=1
+            
+            subStrSet.add(s[right])
+            maxLength = max(maxLength, right-left+1)
+
+        return maxLength
+
+def lengthOfLongestSubstring(self, s):
         n=len(s)
         l,maxLen=0,0
         subStrSet=set()
@@ -13,10 +51,5 @@ class Solution(object):
                     l+=1
                 subStrSet.add(s[r])
         return maxLen
-                    
-    
-# s = "abcabcbb"
-# s = "bbbbb"
-s = "pwwkew"
-obj=Solution()
-print(obj.lengthOfLongestSubstring(s))
+
+"""
